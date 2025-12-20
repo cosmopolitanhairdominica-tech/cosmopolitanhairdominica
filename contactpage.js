36e7a8576deb27b42af6435386bba59256dad5cd
cosmopolitanhairdominica-tech <cosmopolitanhairdominica@gmail.com>
2025-12-20 12:17:10 -0400

    Update contactpage.js

--- contactpage.js
+++ contactpage.js
@@ -1,10 +1,1 @@
-fetch("https://formspree.io/f/abcdwxyz", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    firstName: "Catherine",
-    lastName: "Phillip",
-    email: "catherinap@gmail.com",
-    message: "Hello!"
-  })
-});
+
